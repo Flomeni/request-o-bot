1ab504fdba181bf93022bf90326f7ebfad1b7d75
Max Bangson <flomeni@gmail.com>
2021-05-20 22:03:15 +0300

    0.0.4

--- scripts/build.js
+++ scripts/build.js
@@ -1,23 +1,26 @@
-const fs = require('fs')
 const cmd = require('node-cmd');
-const appRootPath = require('app-root-path').path;
-const packageJSONPath = `${appRootPath}/package.json`;
-const packageJSON = require(packageJSONPath);
-
-const choices = {
-    major: 'major',
-    minor: 'minor',
-    patch: 'patch'
-};
-const PROMPT_NAME = 'VERSIONS';
 
 (function main() {
-    cmd.runSync('npm version patch');
-    //bump();
+    bump();
 })();
 
+async function bump() {
+    const version = await prompt();
+    
+    if (!version) {
+        throw new Error('Your some how managed to not input a version');
+    }
+
+    cmd.runSync(`npm version ${version}`);
+}
 
 async function prompt() {
+    const PROMPT_NAME = 'VERSIONS';
+    const choices = {
+        major: 'major',
+        minor: 'minor',
+        patch: 'patch'
+    };
     const inquirer = require('inquirer');
 
     return inquirer
@@ -36,31 +39,4 @@ async function prompt() {
         .then(answers => {
             return answers[PROMPT_NAME];
         });
-}
-
-function incrementVersion(oldVersion, versionToIncrement) {
-    const verArray = oldVersion.split('.');
-    switch(versionToIncrement){
-        case choices.patch:
-            verArray[2] = `${++verArray[2]}`;
-            break;
-        case choices.minor:
-            verArray[1] = `${++verArray[1]}`;
-            break;
-        case choices.major:
-            verArray[0] = `${++verArray[0]}`
-            break;
-    }
-
-    return verArray.join('.');
-}
-
-async function bump() {
-    const versionToIncrement = await prompt();
-    try {
-        packageJSON.version = incrementVersion(packageJSON.version, versionToIncrement);
-        fs.writeFileSync(packageJSONPath, JSON.stringify(packageJSON, /*replacer*/null, /*space*/2));
-    } catch (e) {
-        console.error(e);
-    }
 }
